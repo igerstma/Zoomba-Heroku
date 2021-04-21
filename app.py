a9ca338ee0736d1e500a79f65a0785f5e2538788
Isaac Gerstmann <Isaac Gerstmann>
2021-04-21 15:51:29 -0600

    Cleaned up app.py

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
-from flask import g
+from flask import Flask, request,  jsonify #, g
 import json
 
 app = Flask(__name__)
 
-# def get_color():
-#     if 'color' not in g:
-#         g.color = "black"
-#     return g.color
-direction = "null"
+direction = "stop"
 
 @app.route('/',methods = ['POST', 'GET'])
 def result():
@@ -19,22 +12,13 @@ def result():
 
     if request.method == 'POST':
         content = request.get_json(force=True)
-        # with app.app_context(): 
         direction = content["Button"]
-        # g['color'] = content["Button"]
-        print("content[Button] = " + content["Button"], "\n\tg.direction = ", direction)
-
         return jsonify(content)
 
     if request.method == 'GET':
-        # with app.app_context(): 
         c = direction
-        direction = "null"
-        # c = g.color
-        print("got direction: " + c)
+        direction = "stop"
         return c
-
-
 
 if __name__ == "_main_":
     app.run(debug=True)
